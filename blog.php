caf73951be52dd174f24a64117df6e34b09ef5e5
Ishika Panwar <ishkipanwar@gmail.com>
2021-01-13 18:20:10 +0530

    blog-timeline

---
+++ blog.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>ZINE | Robotics and Research Group - Where imagination leads to creation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Zine - The only active robtics and research group MNIT for the last 7 years,since its foundation. We have participated in various nationals and internationals events related to a multitude of fields and have gained expertise in various concerned technologies areas. We have also been equally active in inter disciplinary research, at par with state of the art work going in diversified domains. Consistent team work coupled with perseverance, proper guidance, stimulating challenges have gained us numerous significant achievements.">
+    <meta name="keywords" content="Robotics and Research">
+    <meta name="author" content="ZINE">
+    <meta property="og:title" content="ZINE" />
+    <meta property="og:type" content="website" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="fossasia.ico" type="image/x-icon" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/pe-icon-7-stroke.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/theme-lava.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/custom-blog.css" rel="stylesheet" type="text/css" media="all" />
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="images/zine.png">
+	<link rel="stylesheet" href="css/reset_bscktotop.css"> <!-- CSS reset -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+        <script src="js/slider.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/style_backtotop.css"> <!-- Gem style -->
+    <link rel="stylesheet" href="timeline/css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="timeline/css/style_timeline.css"> <!-- Resource style -->
+        <link rel="stylesheet" href="css/slider.css">
+        
+	<script src="timeline/js/modernizr.js"></script> <!-- Modernizr -->
+    </head>
+
+    
+	<body class="no-loader">
+        <div class="loader">
+    		<div class="strip-holder">
+				<div class="strip-1"></div>
+				<div class="strip-2"></div>
+				<div class="strip-3"></div>
+    		</div>
+    	</div>
+
+    	<a id="top"></a>
+
+		<div class="nav-container">
+
+				<nav class="overlay-nav">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-2">
+								<a  target="_self" class="inner-link" href="index.php"><img alt="ZINE" class="logo logo-light" src="img/ZINE/zine1.png">
+								<img alt="ZINE" class="logo logo-dark" src="img/ZINE/zine1.png"></a>
+							</div>
+
+							<div class="col-md-10 text-right">
+								<ul class="menu">
+                                    <li><a target="_self" href="index.php">Home</a></li>
+									<li><a target="_self" href="team.php">Team</a></li>
+									<li><a target="_self" href="project/Projects.php">Projects</a></li>
+									<li><a target="_self" href="achievements.php">Achievements</a></li>
+                                    <li><a target="_self" href="about.php">About</a></li>	
+                                    <li><a target="_self" href="blogs.php">Blogs</a></li>	
+									<li class="social-link hidden-md hidden-sm hidden-xs"><a target="_blank" href="https://www.facebook.com/ROBOTICS.ZINE"><i class="icon social_facebook"></i></a></li>
+      								<li class="social-link hidden-md hidden-sm hidden-xs"><a target="_blank" href="https://www.instagram.com/zine.robotics"><i class="icon social_instagram"></i></a></li>
+                                    		<li class="social-link hidden-md hidden-sm hidden-xs"><a target="_blank" href="https://www.linkedin.com/company/de-zine-limited"><i class="icon social_linkedin"></i></a></li>
+								    
+								    <div class="mobile-menu-toggle"><i class="icon icon_menu"></i></div>
+								</ul>
+							</div>
+						</div>
+					</div>
+					
+					<div class="bottom-border"></div>
+				</nav>
+        </div>
+    
+
+<section class="hero-slider lazy-section">
+				<ul class="slides">
+					<li class="hero-slide">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" data-src="img/ZINE/achie/1.JPG">
+						</div>
+					</li>
+
+					<li class="hero-slide">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" data-src="img/ZINE/achie/2.jpeg">
+						</div>
+					</li>
+
+					<li class="hero-slide">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" data-src="img/ZINE/achie/3.jpeg">
+                        </div>
+					</li><li class="hero-slide">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" data-src="img/ZINE/achie/4.JPG">
+						</div>
+					</li>
+				</ul>
+			</section>
+                  
+            
+    <section id="cd-timeline" class="cd-container">
+
+    <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+            <img src="img/cd-icon-research.svg" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Basic Electrical and Electronics</h2>
+				<!-- <p>
+					2nd Place in Pro Class Race
+			
+
+					<br>Team Members : Devnath Nair
+				</p> -->
+				<span class="cd-date">15th Jan</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+            <img src="img/cd-icon-research.svg" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Algo Pseudo</h2>
+				<!-- <p>
+					1st Place
+			
+
+					<br>Team Members : Nimesh Khandelwal
+				</p> -->
+				<span class="cd-date">18th Jan</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->		
+
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+            <img src="img/cd-icon-research.svg" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Basic Mechanical Engineering</h2>
+				<!-- <p>
+					1st Place<br>
+					3rd Place
+
+					<br>Team Members : Devnath Nair, Vishal Kothari
+				</p> -->
+				<span class="cd-date">21st Jan</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->		
+
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-purpul">
+            <img src="img/cd-icon-research.svg" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>IC-MCU and Sensors</h2>
+				<!-- <p>
+					2nd Place<br>
+					3rd Place
+
+					<br>Team Members : Devnath Nair, Nimesh Khandelwal
+				</p> -->
+				<span class="cd-date">24th Jan</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-orange">
+            <img src="img/cd-icon-research.svg" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Aeromodelling</h2>
+				<!-- <p>
+					2nd Place in Pro Class Race
+					<br>Team Members : Devnath Nair
+				</p> -->
+				<span class="cd-date">27th Jan</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		
+	
+
+            
+	</section> <!-- cd-timeline -->
+        </div>
+
+        <a id="activities" class="in-page-link"></a>
+        <section class="duplicatable-content visitor-info lazy-section">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12 text-center">
+								<h1 class="large-h1">ZINE Activities</h1>
+								<br>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4 col-sm-6">
+								<div class="info-box">
+									<img alt="Venue" data-src="img/ZINE/compet.jpg">
+									<h3>Participates in Various Competitions</h3>
+									<p>Our team has participated and emerged victorious in various national and international robotics events for the past decade owing to the skills and commitment of our members. We firmly believe that having participated in various national and international level events has lended us valuable experience and a knack for victory competing alongside the most premiere robotics teams of India.</p>
+                                    <div><br></div><div><br></div>
+                                    <div class="text-link">
+									<a href="BEE.php" target="_blank">Find Out About Achievements</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+                </div>
+				</section>
+	
+  
+ <!--<div class="modal fade" id="contact_submit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  	<div class="modal-dialog">
+    	<div class="modal-content">
+    		<div class="modal-header">
+            	 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h2>Thank You</h2>
+            </div>
+            <div class="modal-body">
+               <button type="button" class="btn btn-danger" data-dismiss="modal"><center>Close</center></button>
+			</div>
+	</div>
+  </div>   
+</div>    
+--> 
+<?php include('footer-1.php') ?>
+</html>
