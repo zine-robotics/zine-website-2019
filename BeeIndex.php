f43fe25709a3467d6b68363a4020ae7cd3dbc325
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-01-14 18:46:29 +0530

    add beeindex.php

--- BeeIndex.php
+++ BeeIndex.php
@@ -1,51 +1,178 @@
 <?php include('header.php') ?>
-  
 
-<div class="headImage"  >
-    
-   <div class="heading">
-   Basic Electrical and Electronics
+
+<div class="headImage">
+
+	<div class="heading">
+		Basic Electrical and Electronics
+	</div>
 </div>
-</div>   
 <div class="page">
 
-<section class="duplicatable-content visitor-info lazy-section">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-12 text-center">
-								<h1 class="large-h1">BLOGS</h1>
-								<br>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-4 col-sm-6" class="card1">
-								<div class="info-box">
-									<img alt="Venue" data-src="img/ee.jpg">
-									<h3>Introduction</h3>
-									<p>Our team has participated and emerged victorious in various national and international robotics events for the past decade owing to the skills and commitment of our members. We firmly believe that having participated in various national and international level events has lended us valuable experience and a knack for victory competing alongside the most premiere robotics teams of India.</p>
-                                    <div><br></div><div><br></div>
-                                    <div class="text-link">
-									<a href="./Index.php">Go to blog</a>
-									</div>
-								</div>
-                            </div>
-                            <div class="col-md-4 col-sm-6" class="card1">
-								<div class="info-box">
-									<img alt="Venue" data-src="img/ee.jpg">
-									<h3>Circuit Elements</h3>
-									<p>Our team has participated and emerged victorious in various national and international robotics events for the past decade owing to the skills and commitment of our members. We firmly believe that having participated in various national and international level events has lended us valuable experience and a knack for victory competing alongside the most premiere robotics teams of India.</p>
-                                    <div><br></div><div><br></div>
-                                    <div class="text-link">
-									<a href="./circuitElements.php">Go to blog</a>
-									</div>
-								</div>
-							</div>
+	<section class="duplicatable-content visitor-info lazy-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<h1 class="large-h1">BLOGS</h1>
+					<br>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/beeintro.jpg">
+						<h3>Introduction</h3>
+						<p>Robotics is the intersection of science, engineering, and technology that produces machines,
+							called robots, that substitute for (or replicate) human actions. Simple isn't it. Earlier,
+							it was meant to be used in the manufacturing sector only. Now, people are using robots at
+							home, in hospitals, airports, and many more. So, let's begin our journey.
+						</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./Index.php">Go to blog</a>
 						</div>
 					</div>
-                </div>
-				</section>
-	
-  
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/circuitelements.jpg">
+						<h3>Circuit Elements</h3>
+						<p>And the journey starts😀. Let’s start with some conceptual things. So in this blog, we will
+							study circuit elements. What are these and where do we see them in daily life and
+							robotics?🤔
+
+							But before starting with circuit elements, do you know what a circuit is?
+							A circuit is an arrangement of individual electronic components, such as resistors,
+							transistors, capacitors, inductors, and diodes, connected by conductive wires or traces
+							through which electric current can flow.
+						</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/diode.png">
+						<h3>Diodes</h3>
+						<p>In simple words, diode or di-ode or di-electrode is an electronic component that conducts
+							current primarily in one direction; it has low (ideally zero) resistance in one direction,
+							and high (ideally infinite) resistance in the other.
+							A semiconductor diode, the most commonly used type today, is a crystalline piece of
+							semiconductor material with a PN-junction connected to two electrical terminals.
+						</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/switch.jpg">
+						<h3>Switch and Relays</h3>
+						<p>Yes! Switches! It seems such a normal thing. We use a switch to turn on or off any electrical
+							device. Look at that power board in front of you. Yes, It is a switch. But is this
+							it?🙄Pffff….No, obviously. And these are too. There’s much more to it. Let’s start
+							scratching this simple topic and see what all magic it
+							can do. A Switch is a device that breaks(sometimes diverts🙄) the flow of current in a
+							circuit.
+
+						</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/transistor.jpg">
+						<h3>Transistors</h3>
+						<p>Imagine a machine that is as big as MNIT’s VLTC and all you could use it for is to send a
+							text message to your friend , sounds absurd, right?
+
+							Well, that would be the case if transistors weren’t invented, so before you proceed further
+							reading this blog on your phone/laptop don’t forget to thank the noble scientists at Bell
+							Labs who invented it. Well, what exactly is a transistor?
+						</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/transformer.jpg">
+						<h3>Transformers</h3>
+						<p>Remember those huge brown boxes with lots of wiring, that you were always warned not to go
+							near. That was a transformer! These transformers form a crucial part of most circuits from
+							the smallest ones to even power grids. They range in size from RF transformers being less
+							than a cubic centimetre in volume, to units weighing hundreds of tons</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/wires.jpg">
+						<h3>Wires</h3>
+						<p>The very first thing you would notice if you were to enter the zine lab for the first time is
+							that there are several reels of different types of wires all around. Wires and wiring are at
+							the very centre of BEE and robotics. They can very well be the difference between winning
+							and losing in competitions.</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/battery.jpg">
+						<h3>Batteries</h3>
+						<p>If you are getting interested in robotics, you are going to need a lot of different types of
+							batteries for different uses, you'll need to know how to use them and understand the various
+							ratings and specs. There are primary, secondary batteries and much more. Why do we need batteries?</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6" class="card1">
+					<div class="info-box">
+						<img alt="Venue" data-src="img/instruments.jpg">
+						<h3>Instruments</h3>
+						<p>Now we are towards the end of our blogs on the heart of robotics. In this blog you are going
+							to learn about various instruments which you are going to use to make your little robots
+							ready for a fight in the arena. In electronics, joints are made with the help of a process called soldering.
+						</p>
+						<div><br></div>
+						<div><br></div>
+						<div class="text-link">
+							<a href="./circuitElements.php">Go to blog</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+</section>
+
+
 
 </div>
 
@@ -53,4 +180,5 @@
 
 
 <?php include('footer-1.php') ?>
-</html>    
+
+</html>
