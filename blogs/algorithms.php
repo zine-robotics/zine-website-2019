d362fb66196a62b17dd75206bba832c5bde50945
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-01-17 21:16:40 +0530

    algo init

---
+++ blogs/algorithms.php
@@ -0,0 +1,36 @@
+<?php include('header.php') ?>
+
+
+<div class="headImage">
+</div>
+<div class="heading">
+    Concept of Memory and Binary
+</div>
+
+<div class="page">
+    <div class="blog-page">
+    Now that we have understood the basics of how information is actually stored in a computer;  let's get right into the crux of this blog. ALGORITHMS !
+<br><br>
+       
+        <table style="border: 2px solid black;">
+            <tr>
+                <td>
+                    Mail your assignment answers to <a href="mailto:rahul@zine.co.in">rahul@zine.co.in</a> and contact
+                    us in communication channel with doubts.
+                </td>
+            </tr>
+        </table>
+        <br><br><br>
+
+        <ul class="pager">
+            <li class="next"><a href="../circuitElements.php">Next</a></li>
+        </ul>
+
+
+
+    </div>
+</div>
+
+<?php include('footer-1.php') ?>
+
+</html>
---
+++ blogs/header.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>ZINE | Robotics and Research Group - Where imagination leads to creation</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Zine - The only active robtics and research group MNIT for the last 7 years,since its foundation. We have participated in various nationals and internationals events related to a multitude of fields and have gained expertise in various concerned technologies areas. We have also been equally active in inter disciplinary research, at par with state of the art work going in diversified domains. Consistent team work coupled with perseverance, proper guidance, stimulating challenges have gained us numerous significant achievements.">
+        <meta name="keywords" content="Robotics and Research">
+        <meta name="author" content="ZINE">
+        <meta property="og:title" content="ZINE" />
+        <meta property="og:type" content="website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+        <link href="../css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/pe-icon-7-stroke.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/theme-lava.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/custom-blog.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/font-awesome.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="../css/blogs.css" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+        <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+        <!-- CSS only -->
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+      <style type="text/css">@import url('https://themes.googleusercontent.com/fonts/css?kit=iMc4-4exjgqA-toZgqqJBf35jAjY-31lSKWZb9Fa77M');.lst-kix_rcl50rwrr5s8-4>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-4}ol.lst-kix_rcl50rwrr5s8-5.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-5 0}ol.lst-kix_rcl50rwrr5s8-2.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-2 0}.lst-kix_g959rgf1hbpa-7>li:before{content:"\0025cb  "}.lst-kix_g959rgf1hbpa-8>li:before{content:"\0025a0  "}.lst-kix_g959rgf1hbpa-2>li:before{content:"\0025a0  "}.lst-kix_g959rgf1hbpa-3>li:before{content:"\0025cf  "}ol.lst-kix_rcl50rwrr5s8-3.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-3 0}ol.lst-kix_rcl50rwrr5s8-6.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-6 0}.lst-kix_rcl50rwrr5s8-3>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-3}.lst-kix_rcl50rwrr5s8-6>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-6}.lst-kix_g959rgf1hbpa-6>li:before{content:"\0025cf  "}.lst-kix_g959rgf1hbpa-4>li:before{content:"\0025cb  "}.lst-kix_g959rgf1hbpa-5>li:before{content:"\0025a0  "}.lst-kix_g99bis1v6jx-7>li:before{content:"\0025cb  "}.lst-kix_g99bis1v6jx-6>li:before{content:"\0025cf  "}.lst-kix_g99bis1v6jx-8>li:before{content:"\0025a0  "}ol.lst-kix_rcl50rwrr5s8-7.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-7 0}ol.lst-kix_rcl50rwrr5s8-0.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-0 0}.lst-kix_g99bis1v6jx-4>li:before{content:"\0025cb  "}.lst-kix_rcl50rwrr5s8-0>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-0}.lst-kix_g99bis1v6jx-5>li:before{content:"\0025a0  "}ol.lst-kix_rcl50rwrr5s8-4.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-4 0}.lst-kix_g959rgf1hbpa-0>li:before{content:"\0025cf  "}.lst-kix_g959rgf1hbpa-1>li:before{content:"\0025cb  "}.lst-kix_rcl50rwrr5s8-7>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-7,lower-latin) ". "}ul.lst-kix_g959rgf1hbpa-3{list-style-type:none}.lst-kix_rcl50rwrr5s8-7>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-7}ol.lst-kix_rcl50rwrr5s8-8{list-style-type:none}ul.lst-kix_g959rgf1hbpa-2{list-style-type:none}ul.lst-kix_g959rgf1hbpa-1{list-style-type:none}ul.lst-kix_g959rgf1hbpa-0{list-style-type:none}.lst-kix_rcl50rwrr5s8-5>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-5,lower-roman) ". "}ul.lst-kix_g959rgf1hbpa-7{list-style-type:none}ul.lst-kix_g959rgf1hbpa-6{list-style-type:none}.lst-kix_rcl50rwrr5s8-6>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-6,decimal) ". "}ul.lst-kix_g959rgf1hbpa-5{list-style-type:none}ul.lst-kix_g959rgf1hbpa-4{list-style-type:none}ul.lst-kix_g959rgf1hbpa-8{list-style-type:none}.lst-kix_rcl50rwrr5s8-1>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-1}.lst-kix_rcl50rwrr5s8-8>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-8,lower-roman) ". "}.lst-kix_rcl50rwrr5s8-2>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-2}ol.lst-kix_rcl50rwrr5s8-8.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-8 0}.lst-kix_rcl50rwrr5s8-1>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-1,lower-latin) ". "}.lst-kix_rcl50rwrr5s8-8>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-8}ol.lst-kix_rcl50rwrr5s8-1.start{counter-reset:lst-ctn-kix_rcl50rwrr5s8-1 0}.lst-kix_rcl50rwrr5s8-2>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-2,lower-roman) ". "}ol.lst-kix_rcl50rwrr5s8-1{list-style-type:none}.lst-kix_rcl50rwrr5s8-3>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-3,decimal) ". "}ol.lst-kix_rcl50rwrr5s8-0{list-style-type:none}ol.lst-kix_rcl50rwrr5s8-3{list-style-type:none}ol.lst-kix_rcl50rwrr5s8-2{list-style-type:none}ol.lst-kix_rcl50rwrr5s8-5{list-style-type:none}ol.lst-kix_rcl50rwrr5s8-4{list-style-type:none}.lst-kix_rcl50rwrr5s8-5>li{counter-increment:lst-ctn-kix_rcl50rwrr5s8-5}li.li-bullet-0:before{margin-left:-18pt;white-space:nowrap;display:inline-block;min-width:18pt}.lst-kix_rcl50rwrr5s8-4>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-4,lower-latin) ". "}ol.lst-kix_rcl50rwrr5s8-7{list-style-type:none}ol.lst-kix_rcl50rwrr5s8-6{list-style-type:none}ul.lst-kix_g99bis1v6jx-8{list-style-type:none}.lst-kix_g99bis1v6jx-0>li:before{content:"\0025cf  "}ul.lst-kix_g99bis1v6jx-7{list-style-type:none}ul.lst-kix_g99bis1v6jx-0{list-style-type:none}.lst-kix_g99bis1v6jx-3>li:before{content:"\0025cf  "}ul.lst-kix_g99bis1v6jx-2{list-style-type:none}ul.lst-kix_g99bis1v6jx-1{list-style-type:none}ul.lst-kix_g99bis1v6jx-4{list-style-type:none}.lst-kix_g99bis1v6jx-1>li:before{content:"\0025cb  "}.lst-kix_g99bis1v6jx-2>li:before{content:"\0025a0  "}ul.lst-kix_g99bis1v6jx-3{list-style-type:none}ul.lst-kix_g99bis1v6jx-6{list-style-type:none}.lst-kix_rcl50rwrr5s8-0>li:before{content:"" counter(lst-ctn-kix_rcl50rwrr5s8-0,decimal) ". "}ul.lst-kix_g99bis1v6jx-5{list-style-type:none}ol{margin:0;padding:0}table td,table th{padding:0}.c22{border-right-style:solid;padding:5pt 5pt 5pt 5pt;border-bottom-color:#000000;border-top-width:1pt;border-right-width:1pt;border-left-color:#000000;vertical-align:top;border-right-color:#000000;border-left-width:1pt;border-top-style:solid;border-left-style:solid;border-bottom-width:1pt;width:264pt;border-top-color:#000000;border-bottom-style:solid}.c26{border-right-style:solid;padding:5pt 5pt 5pt 5pt;border-bottom-color:#000000;border-top-width:1pt;border-right-width:1pt;border-left-color:#000000;vertical-align:top;border-right-color:#000000;border-left-width:1pt;border-top-style:solid;border-left-style:solid;border-bottom-width:1pt;width:249pt;border-top-color:#000000;border-bottom-style:solid}.c30{border-right-style:solid;padding:5pt 5pt 5pt 5pt;border-bottom-color:#000000;border-top-width:1pt;border-right-width:1pt;border-left-color:#000000;vertical-align:top;border-right-color:#000000;border-left-width:1pt;border-top-style:solid;border-left-style:solid;border-bottom-width:1pt;width:522pt;border-top-color:#000000;border-bottom-style:solid}.c7{-webkit-text-decoration-skip:none;color:#000000;font-weight:700;text-decoration:underline;vertical-align:baseline;text-decoration-skip-ink:none;font-size:14pt;font-family:"Open Sans";font-style:normal}.c19{margin-left:360pt;padding-top:0pt;text-indent:36pt;padding-bottom:0pt;line-height:1.15;orphans:2;widows:2;text-align:center}.c18{-webkit-text-decoration-skip:none;color:#000000;text-decoration:underline;vertical-align:baseline;text-decoration-skip-ink:none;font-size:14pt;font-style:normal}.c4{color:#000000;font-weight:400;text-decoration:none;vertical-align:baseline;font-size:11pt;font-family:"Arial";font-style:normal}.c29{-webkit-text-decoration-skip:none;color:#000000;text-decoration:underline;vertical-align:baseline;text-decoration-skip-ink:none;font-size:11pt;font-style:normal}.c2{padding-top:0pt;padding-bottom:0pt;line-height:1.15;orphans:2;widows:2;text-align:left}.c27{padding-top:0pt;padding-bottom:0pt;line-height:1.15;orphans:2;widows:2;text-align:center}.c0{padding-top:0pt;padding-bottom:0pt;line-height:1.15;orphans:2;widows:2;text-align:justify}.c8{color:#000000;text-decoration:none;vertical-align:baseline;font-size:14pt;font-style:normal}.c16{color:#000000;text-decoration:none;vertical-align:baseline;font-size:18pt;font-style:normal}.c6{color:#000000;text-decoration:none;vertical-align:baseline;font-size:11pt;font-style:normal}.c21{-webkit-text-decoration-skip:none;text-decoration:underline;text-decoration-skip-ink:none;font-size:14pt}.c14{border-spacing:0;border-collapse:collapse;margin-right:auto}.c5{padding-top:0pt;padding-bottom:0pt;line-height:1.0;text-align:left}.c11{-webkit-text-decoration-skip:none;color:#1155cc;text-decoration:underline;text-decoration-skip-ink:none}.c12{max-width:522pt;padding:36pt 36pt 18pt 54pt}.c15{font-weight:700;font-family:"Open Sans"}.c17{padding:0;margin:0}.c3{font-weight:400;font-family:"Open Sans"}.c28{border:1px solid black;margin:5px}.c23{color:inherit;text-decoration:inherit}.c10{margin-left:36pt}.c25{padding-left:0pt}.c24{font-size:14pt}.c9{background-color:#ffffff}.c31{margin-left:72pt}.c13{height:0pt}.c1{height:11pt}.c20{font-size:18pt}.title{padding-top:0pt;color:#000000;font-size:26pt;padding-bottom:3pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}.subtitle{padding-top:0pt;color:#666666;font-size:15pt;padding-bottom:16pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}li{color:#000000;font-size:11pt;font-family:"Arial"}p{margin:0;color:#000000;font-size:11pt;font-family:"Arial"}h1{padding-top:20pt;color:#000000;font-size:20pt;padding-bottom:6pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h2{padding-top:18pt;color:#000000;font-size:16pt;padding-bottom:6pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h3{padding-top:16pt;color:#434343;font-size:14pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h4{padding-top:14pt;color:#666666;font-size:12pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h5{padding-top:12pt;color:#666666;font-size:11pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h6{padding-top:12pt;color:#666666;
+        font-size:11pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;font-style:italic;orphans:2;widows:2;text-align:left}</style>
+    </head>
+        <body class="no-loader">
+        <div class="loader">
+    		<div class="strip-holder">
+				<div class="strip-1"></div>
+				<div class="strip-2"></div>
+				<div class="strip-3"></div>
+    		</div>
+    	</div>
+
+    	<a id="top"></a>
+
+		<div class="nav-container">
+
+				<nav class="overlay-nav">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-2">
+								<a  target="_self" class="inner-link" href="index.php"><img alt="ZINE" class="logo logo-light" src="../img/ZINE/zine1.png">
+								<img alt="ZINE" class="logo logo-dark" src="../img/ZINE/zine1.png"></a>
+							</div>
+
+							<div class="col-md-10 text-right">
+								<ul class="menu">
+                                    <li><a target="_self" href="../index.php">Home</a></li>
+									<li><a target="_self" href="../team.php">Team</a></li>
+									<li><a target="_self" href="../project/Projects.php">Projects</a></li>
+									<li><a target="_self" href="../achievements.php">Achievements</a></li>
+                                    <li><a target="_self" href="../about.php">About</a></li>	
+                                    <li><a target="_self" href="../blogs.php">Blogs</a></li>	
+									<li class="social-link hidden-md hidden-sm hidden-xs"><a target="_blank" href="https://www.facebook.com/ROBOTICS.ZINE"><i class="icon social_facebook"></i></a></li>
+      								<li class="social-link hidden-md hidden-sm hidden-xs"><a target="_blank" href="https://www.instagram.com/zine.robotics"><i class="icon social_instagram"></i></a></li>
+                                    		<li class="social-link hidden-md hidden-sm hidden-xs"><a target="_blank" href="https://www.linkedin.com/company/de-zine-limited"><i class="icon social_linkedin"></i></a></li>
+								    
+								    <div class="mobile-menu-toggle"><i class="icon icon_menu"></i></div>
+								</ul>
+							</div>
+						</div>
+					</div>
+					
+					<div class="bottom-border"></div>
+				</nav>
+        </div>
+    
