9dd1933d43ee317d0821b972799c2f0c228327d4
Chirayu Rankawat <57005734+ChirayuR@users.noreply.github.com>
2021-01-17 21:54:14 +0530

    Algo intro
    Introduction to algorithm blog

---
+++ blogs/introduction.php
@@ -0,0 +1,43 @@
+<?php include('header.php') ?>
+
+
+<div class="headImage">
+</div>
+<div class="heading">
+    Introduction
+</div>
+
+<div class="page">
+    <div class="blog-page">
+         <br><br>
+
+         <img src="../img/images/Introalgo/image3.png" class="img-responsive floating-image center">
+         <br><br> 
+         Are you one of those who feel programming languages and algorithms are extremely hard to understand and you have no clue where to even start? Or you have a bit of an idea about them and want to expand your knowledge? Then you are in the right place. Grab a hot cup of coffee and enjoy an introduction to programming and algorithms and a peek behind the curtain on how they actually fall in place in our daily lives and in robotics. We will learn a lot about bits and bytes, flowcharts and pseudocode, programming and it’s various core constructs, and finally algorithms. 
+        <br><br>
+
+        <img src="../img/images/Introalgo/image1.jpg" class="img-responsive floating-image center">
+        <br><br>
+        Before all that, let us first understand why we should learn all this? What is the motivation behind all this?
+        <br><br> Programming and algorithms are essential for everyone, across all fields. This will only be reemphasized as we move further. Some domains where they play a crucial part are Computational Statistics, Artificial Intelligence, Cloud Computing,  Game Development, E-Commerce and Computer Vision.
+        <br>
+        This is only a very small part of their applications. They are essential to so many other fields and hence it is necessary that everyone must know at least the basics of algorithm and programming. On that note, let’s dive right in.  
+        <br><br>
+        <img src="../img/images/Introalgo/image2.png" class="img-responsive floating-image center">
+        <br><br>
+        Have you ever wondered how computers understand what we command them to do? 
+        
+        <br><br><br>
+
+        <ul class="pager">
+            <li class="next"><a href="./memory.php">Next</a></li>
+        </ul>
+
+
+
+    </div>
+</div>
+
+<?php include('footer-1.php') ?>
+
+</html>
