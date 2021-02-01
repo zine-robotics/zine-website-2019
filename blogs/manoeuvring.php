40b9cebcc3f99475f1dd3914b10932412e00882d
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-02-01 15:53:30 +0530

    blog 3

--- blogs/manoeuvring.php
+++ blogs/manoeuvring.php
@@ -4,7 +4,7 @@
 <div class="headAlgoImage">
 </div>
 <div class="heading">
-Maneuvering
+    Maneuvering
 </div>
 
 
@@ -13,100 +13,145 @@ Maneuvering
     <div class="blog-page"><br>
         <div class="blogPageHeading">Center of gravity & Concept of stability:
         </div>
-<br>
-Ever wondered what would happen when you throw your first built paper plane, which is not controlled by you, there is no one sitting on it, and you just throw it? <br>
-Well, after you let go of that plane, it completely depends on the build of your plane how it goes, it will have to face various <strong>disturbances</strong>, and <strong>how your plane reacts</strong>  to it depends on its <strong>design</strong>, and that reaction determines its stability.
- <br><br>
-There are two types of stability we will discuss, one is <strong>static stability</strong>, which is the initial tendency of aircraft to return to its position, completely dependent on its design. While <strong>Dynamic stability</strong> is the stability over the course of time. So, not just intention, not just tendency, but coming back so the outcome of disturbing an aircraft depends on its dynamic stability. There are also other types of stability, roll stability, in yaw, in pitch, of course, we want to cover this type of stability too, but it will start to get boring and a little off-topic. We’ll not go into depth as of now, though you are encouraged to learn further about it <a href="https://drive.google.com/file/d/1Jk2GVwEXlz2zK-hDHXUlYVaDtTNeP4tT/view">here</a>.
-<br><br>
-Intuitively, you might want to balance to maintain stability, so how should we balance? Like Thanos?
-But we don’t want to go intuitively, so we define three major points,i.e, CG, CP, Neutral Point. Let’s start with the <strong>centre of gravity.</strong> We won’t discuss the other two points here. 
-The center of gravity is the average location of the weight of the aircraft. The weight is distributed throughout the airplane, and for some problems, it is important to know the distribution. But for total aircraft maneuvering, we need to be concerned with only the total weight and the location of the center of gravity. 
- 
-<br><br>
-<img src="../img/images/aero/image8.png" class="img-responsive floating-image left">  
-<img src="../img/images/aero/image50.png" class="img-responsive floating-image right">  
-<br><br>
-<br><br><br><br><br><br>
-Incidentally, do you know that <strong>fuel is also stored in the wings</strong> of aircraft? Any reason for that? Find out, it's your assignment.
-<br><br>
-<center>3 MAJOR AXES OF A PLANE	</center> 
-<br>
-<img src="../img/images/aero/image9.png" class="img-responsive floating-image center">  
+        <br>
+        Ever wondered what would happen when you throw your first built paper plane, which is not controlled by you,
+        there is no one sitting on it, and you just throw it? <br>
+        Well, after you let go of that plane, it completely depends on the build of your plane how it goes, it will have
+        to face various <strong>disturbances</strong>, and <strong>how your plane reacts</strong> to it depends on its
+        <strong>design</strong>, and that reaction determines its stability.
+        <br><br>
+        There are two types of stability we will discuss, one is <strong>static stability</strong>, which is the initial
+        tendency of aircraft to return to its position, completely dependent on its design. While <strong>Dynamic
+            stability</strong> is the stability over the course of time. So, not just intention, not just tendency, but
+        coming back so the outcome of disturbing an aircraft depends on its dynamic stability. There are also other
+        types of stability, roll stability, in yaw, in pitch, of course, we want to cover this type of stability too,
+        but it will start to get boring and a little off-topic. We’ll not go into depth as of now, though you are
+        encouraged to learn further about it <a
+            href="https://drive.google.com/file/d/1Jk2GVwEXlz2zK-hDHXUlYVaDtTNeP4tT/view">here</a>.
+        <br><br>
+        Intuitively, you might want to balance to maintain stability, so how should we balance? Like Thanos?
+        But we don’t want to go intuitively, so we define three major points,i.e, CG, CP, Neutral Point. Let’s start
+        with the <strong>centre of gravity.</strong> We won’t discuss the other two points here.
+        The center of gravity is the average location of the weight of the aircraft. The weight is distributed
+        throughout the airplane, and for some problems, it is important to know the distribution. But for total aircraft
+        maneuvering, we need to be concerned with only the total weight and the location of the center of gravity.
 
-We have a three-dimensional world, and thus, we have <strong>three major axes,i.e, lateral, longitudinal, and vertical,</strong> along which motion of an aircraft is controlled (see the section on controlling below). The point of intersection of these three axes gives us the Center of Gravity. The centre of gravity is the point along which the aircraft rotates.
-<br><br>
-Now let’s discuss the body parts of the plane as shown in the above image.
-<br><br>
-<div class="blogPageHeading">Body Parts
-</div>
-<br>
-<img src="../img/images/aero/image29.png" class="img-responsive floating-image center">  
-<br><br>
-<div class="blogPageSubHeading">1. Fuselage:</div>
-<br>
-The fuselage is an aircraft's main body section.
-<br><br>
-<div class="blogPageSubHeading">2. Elavators:</div>
-<br>
-Contains rudder and elevators(both vertical and horizontal). 
-Vertical elevators are also called fins. The vertical stabilizer keeps the nose of the plane from swinging from side to side, which is called yaw.
-<br><br>
-<div class="blogPageSubHeading">3. wings:</div>
-<br>
-Wings are responsible for providing lift to the aircraft.
-<br><br>
-<div class="blogPageSubHeading">4. Ailerons:</div>
-<br>
-Ailerons control the roll of the airplane about its longitudinal axis (imagine a straight line running from nose to tail).
-<br><br>
-Ailerons come in pairs and are found on the trailing (rear) edge of the wing, and they work opposite to each other i.e. when one aileron moves up, the other one moves down and vice versa.
-<img src="../img/images/aero/image35.gif" class="img-responsive floating-image center">  
+        <br><br>
+        <img src="../img/images/aero/image8.png" class="img-responsive floating-image left">
+        <img src="../img/images/aero/image50.png" class="img-responsive floating-image right">
+        <br><br>
+        <br><br><br><br><br><br>
+        Incidentally, do you know that <strong>fuel is also stored in the wings</strong> of aircraft? Any reason for
+        that? Find out, it's your assignment.
+        <br><br>
+        <center>3 MAJOR AXES OF A PLANE </center>
+        <br>
+        <img src="../img/images/aero/image9.png" class="img-responsive floating-image center">
 
-<br><br>
+        We have a three-dimensional world, and thus, we have <strong>three major axes,i.e, lateral, longitudinal, and
+            vertical,</strong> along which motion of an aircraft is controlled (see the section on controlling below).
+        The point of intersection of these three axes gives us the Center of Gravity. The centre of gravity is the point
+        along which the aircraft rotates.
+        <br><br>
+        Now let’s discuss the body parts of the plane as shown in the above image.
+        <br><br>
+        <div class="blogPageHeading">Body Parts
+        </div>
+        <br>
+        <img src="../img/images/aero/image29.png" class="img-responsive floating-image center">
+        <br><br>
+        <div class="blogPageSubHeading">1. Fuselage:</div>
+        <br>
+        The fuselage is an aircraft's main body section.
+        <br><br>
+        <div class="blogPageSubHeading">2. Elavators:</div>
+        <br>
+        Contains rudder and elevators(both vertical and horizontal).
+        Vertical elevators are also called fins. The vertical stabilizer keeps the nose of the plane from swinging from
+        side to side, which is called yaw.
+        <br><br>
+        <div class="blogPageSubHeading">3. wings:</div>
+        <br>
+        Wings are responsible for providing lift to the aircraft.
+        <br><br>
+        <div class="blogPageSubHeading">4. Ailerons:</div>
+        <br>
+        Ailerons control the roll of the airplane about its longitudinal axis (imagine a straight line running from nose
+        to tail).
+        <br><br>
+        Ailerons come in pairs and are found on the trailing (rear) edge of the wing, and they work opposite to each
+        other i.e. when one aileron moves up, the other one moves down and vice versa.
+        <img src="../img/images/aero/image35.gif" class="img-responsive floating-image center">
 
-<div class="blogPageSubHeading">5. Horizontal stabilizer:</div>
-<br>
-Horizontal stabilizers provide stability to the aircraft, avoiding the up-and-downs of aircraft nose and helps aircraft  to fly straight. It is fixed unlike elevators, which are included in stabilizers, which are movable. The horizontal stabilizer prevents an up-and-down motion of the nose, which is called pitch. But how does horizontal stabilizers provide stability? Think!
-<br><br>
-<div class="blogPageSubHeading">6. Vertical stabilizer:</div>
-<br>
-A vertical stabilizer provides directional (see yaw below) stability and usually comprises a fixed fin and movable control rudder hinged to its rear edge. Sometimes, there is no hinge and the whole fin surface is pivoted for both stability and control.
-<br><br>
-<div class="blogPageSubHeading">7. Rudder:</div>
-<br>
-The rudder is the hinged section of the fin, or vertical stabilizer, at the rear of the airplane.
- <br><br>
- It's used for directional control by changing the yaw of the airplane and works in a positive manner i.e. moving the rudder to the left causes the airplane to turn left and vice versa. Learn more about it in the section below.
-<br><br>
-<img src="../img/images/aero/image15.gif" class="img-responsive floating-image center">  
-<br><br>
-<div class="blogPageHeading">Controlling the motion of the flight in aircraft </div><br><br>
+        <br><br>
 
-The directional control of a fixed-wing aircraft takes place around the lateral, longitudinal, and vertical axes employing flight control surfaces designed to create a movement about these axes. These  <strong>control</strong> surfaces are generally movable and hinged objects.
-<br> There are three axes along which we need to control our aircraft, let’s jump in and understand how manoeuvring is done. Take axes as imaginary lines along which the body can rotate.
-<br><br>
-<strong>Elevation of an aircraft</strong>, also known as Pitch, is controlled by <strong>elevators</strong>. As the name implies, the elevator helps “elevate” the aircraft. It is usually located on the tail of the aircraft and serves two purposes. The first is to provide stability by producing a downward force on the tail. Aeroplanes are traditionally nose-heavy (to improve stability from turbulence) and this downward force is required to compensate for that. The second is to direct the nose of the aircraft either upwards or downwards, known as pitch, to make the aeroplane climb and descend.    
-                               
-<br>
-<img src="../img/images/aero/image5.png" class="img-responsive floating-image center">  
+        <div class="blogPageSubHeading">5. Horizontal stabilizer:</div>
+        <br>
+        Horizontal stabilizers provide stability to the aircraft, avoiding the up-and-downs of aircraft nose and helps
+        aircraft to fly straight. It is fixed unlike elevators, which are included in stabilizers, which are movable.
+        The horizontal stabilizer prevents an up-and-down motion of the nose, which is called pitch. But how does
+        horizontal stabilizers provide stability? Think!
+        <br><br>
+        <div class="blogPageSubHeading">6. Vertical stabilizer:</div>
+        <br>
+        A vertical stabilizer provides directional (see yaw below) stability and usually comprises a fixed fin and
+        movable control rudder hinged to its rear edge. Sometimes, there is no hinge and the whole fin surface is
+        pivoted for both stability and control.
+        <br><br>
+        <div class="blogPageSubHeading">7. Rudder:</div>
+        <br>
+        The rudder is the hinged section of the fin, or vertical stabilizer, at the rear of the airplane.
+        <br><br>
+        It's used for directional control by changing the yaw of the airplane and works in a positive manner i.e. moving
+        the rudder to the left causes the airplane to turn left and vice versa. Learn more about it in the section
+        below.
+        <br><br>
+        <img src="../img/images/aero/image15.gif" class="img-responsive floating-image center">
+        <br><br>
+        <div class="blogPageHeading">Controlling the motion of the flight in aircraft </div><br><br>
 
-Next is <strong> rolling</strong>, done by <strong>Ailerons</strong>, The ailerons are located at the rear of the wing, one or more on each side. They work opposite to each other, so when one is raised, the other is lowered. Their job is to increase the lift on one wing while reducing the lift on the other. By doing this, they roll the aircraft sideways, which allows the aircraft to turn. This is the primary method of steering a fixed-wing aircraft. Modern Large Aircraft has 2 ailerons, one Inboard (near fuselage) and other Outboard (at the end of the wings). Why are the two ailerons provided? Think!
-<img src="../img/images/aero/image36.png" class="img-responsive floating-image center">  
+        The directional control of a fixed-wing aircraft takes place around the lateral, longitudinal, and vertical axes
+        employing flight control surfaces designed to create a movement about these axes. These <strong>control</strong>
+        surfaces are generally movable and hinged objects.
+        <br> There are three axes along which we need to control our aircraft, let’s jump in and understand how
+        manoeuvring is done. Take axes as imaginary lines along which the body can rotate.
+        <br><br>
+        <strong>Elevation of an aircraft</strong>, also known as Pitch, is controlled by <strong>elevators</strong>. As
+        the name implies, the elevator helps “elevate” the aircraft. It is usually located on the tail of the aircraft
+        and serves two purposes. The first is to provide stability by producing a downward force on the tail. Aeroplanes
+        are traditionally nose-heavy (to improve stability from turbulence) and this downward force is required to
+        compensate for that. The second is to direct the nose of the aircraft either upwards or downwards, known as
+        pitch, to make the aeroplane climb and descend.
 
-<br><br>                                      
-Next, <strong>yawing</strong>, done by <strong>rudders</strong>, The rudder is located on the tail of the aircraft. It works identically to a rudder on a boat, steering the nose of the aircraft left and right. Unlike the boat, however, it is not the primary method of steering. Its main purpose is to counteract the drag caused by the lowered aileron during a turn. This adverse yaw, as it is known, causes the nose of the aeroplane to point away, or outwards, from the direction of the turn. The rudder helps to correct this by pushing the nose in the correct direction, maintaining what is known as coordinated flight.
-<img src="../img/images/aero/image12.png" class="img-responsive floating-image center">  
-  <br>
-  <img src="../img/images/aero/image27.jpg" class="img-responsive floating-image center">  
-<center>General overview of what we discussed</center>
-        
-         <br><br>           
+        <br>
+        <img src="../img/images/aero/image5.png" class="img-responsive floating-image center">
 
-Watch <a href="https://www.youtube.com/watch?v=XDIg7yVO6zw">this</a> video for demonstration.
+        Next is <strong> rolling</strong>, done by <strong>Ailerons</strong>, The ailerons are located at the rear of
+        the wing, one or more on each side. They work opposite to each other, so when one is raised, the other is
+        lowered. Their job is to increase the lift on one wing while reducing the lift on the other. By doing this, they
+        roll the aircraft sideways, which allows the aircraft to turn. This is the primary method of steering a
+        fixed-wing aircraft. Modern Large Aircraft has 2 ailerons, one Inboard (near fuselage) and other Outboard (at
+        the end of the wings). Why are the two ailerons provided? Think!
+        <img src="../img/images/aero/image36.png" class="img-responsive floating-image center">
+
+        <br><br>
+        Next, <strong>yawing</strong>, done by <strong>rudders</strong>, The rudder is located on the tail of the
+        aircraft. It works identically to a rudder on a boat, steering the nose of the aircraft left and right. Unlike
+        the boat, however, it is not the primary method of steering. Its main purpose is to counteract the drag caused
+        by the lowered aileron during a turn. This adverse yaw, as it is known, causes the nose of the aeroplane to
+        point away, or outwards, from the direction of the turn. The rudder helps to correct this by pushing the nose in
+        the correct direction, maintaining what is known as coordinated flight.
+        <img src="../img/images/aero/image12.png" class="img-responsive floating-image center">
+        <br>
+        <img src="../img/images/aero/image27.jpg" class="img-responsive floating-image center">
+        <center>General overview of what we discussed</center>
+
+        <br><br>
+
+        Watch <a href="https://www.youtube.com/watch?v=XDIg7yVO6zw">this</a> video for demonstration.
 
 
- <br><br>
+        <br><br>
         <ul class="pager">
             <li class="previous"><a href="./memory.php">Previous</a></li>
 
---
+++ blogs/rcplane.php
@@ -0,0 +1,140 @@
+<?php include('header.php') ?>
+
+
+<div class="headAlgoImage">
+</div>
+<div class="heading">
+    RC Plane DIY
+</div>
+
+
+
+<div class="page">
+    <div class="blog-page"><br><br>
+        We have studied the physics and those lengthy theories, we have seen how the plane is controlled, <i>but how
+            about actually building one of those</i>?
+        <br> In this section we will build a Radio Controlled plane.
+        <br><br>
+        Components Required: <br>
+        <b>Frame:</b>
+        <br><br>
+        Arguably the most important part of the entire RC plane has to be the frame. When it comes to making an RC
+        aircraft, choosing the right frame is the first hurdle you need to cross. Our plane will encounter many stresses
+        during its flight, also flying an RC plane takes some time getting used to, so we need to choose the best
+        material for the flight. What are the qualities we can look for? Light-weight, not too pricey, should have good
+        strength. You can use carbon fibre, it is really lightweight and five times stronger than steel! The drawback?
+        It's pricey. So sometimes the fuselage/ only wings are made of carbon fibre.
+        <br> Another material which is more popular, is polystyrene foam, comes under the name of depron, has good
+        rigidity and flexibility. And it is affordable too!
+        <br> Balsa Wood is also used, it is relatively inexpensive, easy to carve.
+
+        <br> <br>
+        <img src="../img/images/aero/image33.png" class="img-responsive floating-image center">
+        <br>
+        <b>Tail:</b> <br><br>
+        Tail is an important part of a RC plane, reason being, they are a crucial factor for stability and
+        maneuverability. It consists of stabilizers, rudder and elevators.
+        <br><br>
+        <b>Motor:</b><br><br>
+        There are two types of motors: Brushed and brushless motors. [Refer to BME]. We generally use brushless motors
+        DC motors(BLDC). A DC motor converts the electric current into Torque and the voltage into rotations per
+        minute(RPM). This motor is used to generate thrust for the aircraft. Learn more <a
+            href="https://rcplanes.online/guide5.htm#:~:text=There%20are%20two%20main%20different,The%20brushed%20and%20the%20brushless.">here</a>.
+        <br><br>
+        <b>ESC:</b><br><br>
+        ESCs or electronic speed controllers, control the power going through motors, convert DC to AC current for
+        brushless motors. Also, Receivers require 5V, so ESCs convert voltage provided by batteries. While choosing an
+        ESC, we should consider its amperage rating, preferably more than that required by motors. Also remember that
+        ESC can get very warm, so it is always advisable to use an ESC with a rating more than that required by the
+        motors.
+        <img src="../img/images/aero/image10.png" class="img-responsive floating-image center">
+        <br><br>
+
+        <b>Assignment:</b> <br><br>
+        Tell us the connection of each wire of esc shown in the diagram with other electronic components of the RC
+        plane.
+        <br><br>
+        <b>Servo:</b><br><br>
+        Servo motors are used for adjusting control surfaces while in motion for maneuvering. They are used because of
+        their ability to stop precisely and hold the position.
+        <img src="../img/images/aero/image45.png" class="img-responsive floating-image center">
+        <br><br>
+
+        <b>Assignment:</b><br><br>
+        Find out how servo motors mechanically control the control surfaces(rudder, elevator, ailerons).
+        Also tell what is the weight of a normal servo motor?
+        <br><br>
+        <b>Battery:</b> <br><br>
+        Generally LiPo batteries are used for RC systems. Each cell of LiPo battery has a nominal voltage of 3.7V and
+        maximum voltage of 4.2V.
+        <br><br>
+        <b>Transmitter-Receiver:</b><br><br>
+        Radio Transmitter is an electronic device that uses radio signals to transmit commands wirelessly via a set
+        radio frequency over to the Radio Receiver, which is connected to our aircraft. In other words, it’s the device
+        that translates pilot’s commands to be received by the receiver.
+        Transmitter (with the pilot) and the Receiver (on the airborne vehicle) work on Electromagnetic signals. A
+        typical RC Transmitter has about 4 to 6 channels with some being proportional, (i.e. controlled surfaces move
+        proportion the movements of the control sticks).
+        <br><br>
+
+
+        So what does a channel mean? Radio Transmitter transmits commands via channels. Each channel is an individual
+        action being sent to the aircraft.
+        <br><br>
+        Throttle, Yaw, Pitch and Roll are the four main inputs required to control the plane. Each of them uses one
+        channel, so there is a minimum of four channels required. Every switch, slider or knob on the transmitter uses
+        one channel to send the information through to the receiver.
+        <br> Additional channels may function in an "on-off" manner for some additional components.
+        <img src="../img/images/aero/image32.jpg" class="img-responsive floating-image center">
+
+        <br><br>
+        A receiver on the other hand facilitates and communicates the transmitter instructions of the pilot
+        <img src="../img/images/aero/image7.jpg" class="img-responsive floating-image left">
+
+        Receiver passes on the signal to the flight controller to be further processed. <br>
+        A Receiver must be compatible with the Radio Transmitter which in most cases means that the same brand of Rx and
+        Tx need to be purchased in order to establish a communication. There are however radio receivers that may work
+        with the same protocol but are not from the same brand.
+        <br><br> Frequencies (oscillation rate at which these radios work) must also be the same on both Rx and Tx. For
+        instance; a 2.4GHz Transmitter can only work with a 2.4GHz Radio Receiver.
+
+
+        <b><i>How do these components fit together?</i></b> <br>
+        <a href="https://www.youtube.com/watch?v=KJWg5HJBGGc">Here</a> is an excellent tutorial.
+        <br><br>
+
+
+        <div class="blogPageSubHeading">Propulsion System </div> <br>
+        A propulsion system is a machine that produces thrust to push an object forward.
+        <br><br>
+
+        <div class="blogPageSubHeading"> Types of propulsion systems:-</div>
+        <br><br>
+        <b>1.Tractor</b> : Such propulsion systems have propellers in front
+        <img src="../img/images/aero/image42.png" class="img-responsive floating-image center">
+
+        <br>
+
+        <center> It has propeller at front</center>
+        <br><br>
+        <b>2. Pusher</b> : Such propulsion systems have propellers at their back.
+        <img src="../img/images/aero/image24.png" class="img-responsive floating-image center">
+
+        <center>Propeller fans are at back of plane</center>
+
+
+        <br><br>
+        <ul class="pager">
+            <li class="previous"><a href="./memory.php">Previous</a></li>
+
+            <li class="next"><a href="./pseudo.php">Next</a></li>
+        </ul>
+
+
+
+    </div>
+</div>
+
+<?php include('footer-1.php') ?>
+
+</html>
