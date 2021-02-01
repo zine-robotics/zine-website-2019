9eef9266bf70574716f847336f028414b39d22cd
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-02-01 15:35:43 +0530

    aero blog 2

--- blogs/ic-mcu-index.php
+++ blogs/ic-mcu-index.php
@@ -5,7 +5,7 @@
 <section class="hero-slider lazy-section">
 	<ul class="slides">
 
-	<li class="hero-slide">
+		<li class="hero-slide">
 			<div class="background-image-holder parallax-background">
 				<img class="background-image" alt="Background Image" data-src="../img/ic-sl1.jpg">
 			</div>
@@ -46,7 +46,7 @@
 			</div>
 		</li>
 
-		
+
 	</ul>
 </section>
 <div class="page">
@@ -62,11 +62,14 @@
 			<div class="row">
 				<div class="col-md-4 col-sm-6" class="card1">
 					<div class="info-box">
-						<img alt="Venue" data-src="../img/images/more-ic/download.jpeg" style="height:240px; width:380px;">
+						<img alt="Venue" data-src="../img/images/more-ic/download.jpeg"
+							style="height:240px; width:380px;">
 						<h3>Intro and IC's</h3>
-						<p> We’ll first introduce the ICs and then we’ll dive deep into what types of ICs we have, how they are classified, how an IC looks, and pretty much everything necessary for you to know.
-Then we shall continue with the processing unit in which you’ll get along with microcontrollers and the genius saints sitting inside the core of computers.  
-And then, we’ll descend into the sensors, their types, classification, etc.
+						<p> We’ll first introduce the ICs and then we’ll dive deep into what types of ICs we have, how
+							they are classified, how an IC looks, and pretty much everything necessary for you to know.
+							Then we shall continue with the processing unit in which you’ll get along with
+							microcontrollers and the genius saints sitting inside the core of computers.
+							And then, we’ll descend into the sensors, their types, classification, etc.
 
 						</p>
 						<div><br></div>
@@ -80,8 +83,11 @@ And then, we’ll descend into the sensors, their types, classification, etc.
 					<div class="info-box">
 						<img alt="Venue" data-src="../img/images/more-ic/image15.gif">
 						<h3>More about IC's</h3>
-						<p>When you work with ICs, you need to know the difference between various ICs to ensure that you choose the correct IC you need for your work.
-                         When you take a look at these tiny black chips, you might find it back-breaking to differentiate them from the ICs lying around. Not to worry, we will learn about different IC's in this blog.
+						<p>When you work with ICs, you need to know the difference between various ICs to ensure that
+							you choose the correct IC you need for your work.
+							When you take a look at these tiny black chips, you might find it back-breaking to
+							differentiate them from the ICs lying around. Not to worry, we will learn about different
+							IC's in this blog.
 						</p>
 						<div><br></div>
 						<div><br></div>
@@ -95,10 +101,12 @@ And then, we’ll descend into the sensors, their types, classification, etc.
 						<img alt="Venue" data-src="../img/images/diff-IC/image8.png">
 						<h3>Different IC's and their usage</h3>
 						<p>
-                        ICs not only revolutionized the field of robotics but also innovated electronics to great extent.
-                        Now that we know how fabulous ICs are, let’s dive into their usage in robotics.
-                        ICs are used for basic decision-making in robots and some are used in circuits for voltage regulation, amplification, calculating time
-                        while some are used for handling heavy processing tasks for robots.
+							ICs not only revolutionized the field of robotics but also innovated electronics to great
+							extent.
+							Now that we know how fabulous ICs are, let’s dive into their usage in robotics.
+							ICs are used for basic decision-making in robots and some are used in circuits for voltage
+							regulation, amplification, calculating time
+							while some are used for handling heavy processing tasks for robots.
 
 
 						</p>
@@ -113,9 +121,12 @@ And then, we’ll descend into the sensors, their types, classification, etc.
 					<div class="info-box">
 						<img alt="Venue" data-src="../img/images/more-ic/mcu.gif" style="height:280px; width:350px;">
 						<h3>Microcontroller and Microprocessors</h3>
-						<p>We’ve been this far but we still don’t know how a robot makes decisions and processes data? Logic Gate ICs? But didn’t they make an ambiguous,
-                         huge, and complex circuit even just to do simple operations like additions? There are devices available that process data. Take your phone, for example,
-                         it can do a lot of operations rather than simple addition in such a small size. What gave this device such powerful abilities?
+						<p>We’ve been this far but we still don’t know how a robot makes decisions and processes data?
+							Logic Gate ICs? But didn’t they make an ambiguous,
+							huge, and complex circuit even just to do simple operations like additions? There are
+							devices available that process data. Take your phone, for example,
+							it can do a lot of operations rather than simple addition in such a small size. What gave
+							this device such powerful abilities?
 
 						</p>
 						<div><br></div>
@@ -129,10 +140,13 @@ And then, we’ll descend into the sensors, their types, classification, etc.
 					<div class="info-box">
 						<img alt="Venue" data-src="../img/images/more-ic/ard.jpeg" style="height:240px; width:280px;">
 						<h3>Development Boards</h3>
-						<p>Here we are with another fascinating topic for you to learn - Development Boards, more specifically, Arduino Uno. 
-                        Arduino boards are the pivotal components for thousands of ongoing projects in the field of robotics. With Arduino boards, you can convert input,
-                         like light, temperature, etc., 
-                        from the sensors and govern  an output, like activating an arm, a motor, or even sending information online.</p>
+						<p>Here we are with another fascinating topic for you to learn - Development Boards, more
+							specifically, Arduino Uno.
+							Arduino boards are the pivotal components for thousands of ongoing projects in the field of
+							robotics. With Arduino boards, you can convert input,
+							like light, temperature, etc.,
+							from the sensors and govern an output, like activating an arm, a motor, or even sending
+							information online.</p>
 						<div><br></div>
 						<div><br></div>
 						<div class="text-link">
@@ -144,8 +158,10 @@ And then, we’ll descend into the sensors, their types, classification, etc.
 					<div class="info-box">
 						<img alt="Venue" data-src="../img/images/more-ic/ard2.png" style="height:240px; width:400px;">
 						<h3>Programming Arduino Uno</h3>
-						<p>Now let’s start programming our Arduino board. Generally, we code in Arduino IDE, compile it and upload it to the Arduino. And the Arduino mostly behaves as expected. Simple?
-                         The programming language we use for programming the Arduino devices is Arduino Programming Language.
+						<p>Now let’s start programming our Arduino board. Generally, we code in Arduino IDE, compile it
+							and upload it to the Arduino. And the Arduino mostly behaves as expected. Simple?
+							The programming language we use for programming the Arduino devices is Arduino Programming
+							Language.
 						</p>
 						<div><br></div>
 						<div><br></div>
@@ -191,9 +207,7 @@ fields of electronics also have several sensors so that the ICs and microcontrol
 					</div>
 				</div>
 			</div>
-		</div>
-</div>
-</section>
+	</section>
 
 
 
